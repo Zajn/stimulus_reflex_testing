f5775fe37384015471bb4bb2b42a0f6349ff6da1
Zachary T Jones <zt.jones88@gmail.com>
2022-08-17 02:27:13 -0400

    WIP

--- test/stimulus_reflex_testing_test.rb
+++ test/stimulus_reflex_testing_test.rb
@@ -5,17 +5,31 @@ class StimulusReflexTestingTest < Minitest::Test
     refute_nil ::StimulusReflexTesting::VERSION
   end
 
-  def test_includes_version_number
+  def test_includes_version_number_for_sr_350pre9_and_newer
     mock = MiniTest::Mock.new
     opts = { channel: 'TestChannel', element: 'TestElement', url: 'https://test.xyz', method_name: 'test' }
+
     mock.expect(:call, true) do |channel, element:, url:, method_name:, params:, client_attributes:|
-      assert_equal(::StimulusReflex::VERSION, client_attributes[:version])
+      assert_equal('3.5.0pre9', client_attributes[:version])
     end
 
     TestReflexClass.stub(:new, mock) do
-      TestClass.new.build_reflex(opts)
+      TestClass.new.build_reflex(opts, '3.5.0pre9')
     end
     mock.verify
+  end
+
+  def test_does_not_include_version_number_for_sr_350pre8_and_older
+    mock = MiniTest::Mock.new
+    opts = { channel: 'TestChannel', element: 'TestElement', url: 'https://test.xyz', method_name: 'test' }
+
+    mock.expect(:call, true) do |channel, element:, url:, method_name:, params:, client_attributes:|
+      assert_nil(client_attributes[:version])
+    end
+
+    TestReflexClass.stub(:new, mock) do
+      TestClass.new.build_reflex(opts, '3.5.0pre8')
+    end
   end
 end
 
